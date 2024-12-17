81639c40d05e83c485752b4014358d4090bb3cc7
kusumachandu <kusumachandu43@gmail.com>
2024-12-17 18:19:14 +0530

    fixed bugs on panel plate, title and screen name changes, image upload and pdf formatter

--- panel source/src/App.jsx
+++ panel source/src/App.jsx
@@ -1,27 +1,19 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Panel from "./Panel";
 
- function App() {
-
+function App() {
   return (
     <Router>
-      <ToastContainer />
       <Routes>
         <Route path="/" element={<Panel />} />
         <Route path="/:Id" element={<Panel />} />
         {/* <Route path="/page" element={<Page />} /> */}
- 
-      </Routes> 
+      </Routes>
     </Router>
   );
 }
 
-
-
-
-export default App
+export default App;
